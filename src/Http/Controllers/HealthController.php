d748bac2abd25578d1b3da5c15df0487dc7732a9
Mohammadreza_73 <mohammadreza.rahimi93@yahoo.com>
2023-01-04 02:01:37 +0330

    Add check db connection

--- src/Http/Controllers/HealthController.php
+++ src/Http/Controllers/HealthController.php
@@ -3,11 +3,36 @@
 namespace LaravelHealth\Http\Controllers;
 
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
 
 class HealthController extends Controller
 {
     public function index()
     {
-        //
+        $data = [];
+        if (config('health.database')) {
+            $database = [
+                'status' => true,
+                'data' => null
+            ];
+
+            if (! self::checkDataBase()) {
+                $database['status'] = false;
+                $database['data'] = 'اتصال به دیتابیس امکان‌پذیر نیست';
+            }
+            $data['database'] = $database;
+        }
+
+        return view('health::index', compact('data'));
+    }
+
+    private static function checkDataBase()
+    {
+        try {
+            DB::connection()->getPdo();
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
     }
 }
