75a4af659dcddf4c9933831a30130d391c21a15e
Mohammadreza_73 <mohammadreza.rahimi93@yahoo.com>
2023-01-06 23:47:19 +0330

    Modify health controller:
    Add check migrations,
    Add check routes.

--- src/Http/Controllers/HealthController.php
+++ src/Http/Controllers/HealthController.php
@@ -4,6 +4,9 @@ namespace LaravelHealth\Http\Controllers;
 
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Storage;
 
 class HealthController extends Controller
 {
@@ -23,9 +26,40 @@ class HealthController extends Controller
             $data['database'] = $database;
         }
 
+        if (config('health.migrations')) {
+            $migrations = [
+                'status' => true,
+                'data' => null,
+            ];
+
+            if (! self::checkMigrataions()) {
+                $migrations['status'] = false;
+                $migrations['data'] = 'در اجرا مایگریشن‌ها مشکلی وجود دارد';
+            }
+            $data['migrations'] = $migrations;
+        }
+
+        if (config('health.routes')) {
+            $routes = [
+                'status' => true,
+                'data' => null,
+            ];
+
+            if (! self::checkRoutes()) {
+                $routes['status'] = false;
+                $routes['data'] = 'روت‌ها در درسترس نیستند';
+            }
+            $data['routes'] = $routes;
+        }
+
         return view('health::index', compact('data'));
     }
 
+    /**
+     * Check database connection
+     *
+     * @return bool
+     */
     private static function checkDataBase()
     {
         try {
@@ -34,5 +68,48 @@ class HealthController extends Controller
         } catch (\Exception $e) {
             return false;
         }
+    }
+
+    /**
+     * Check migrations
+     *
+     * @return bool
+     */
+    private static function checkMigrataions()
+    {
+        $migrations = DB::table('migrations')->get();
+        $files = Storage::files(base_path() . '/database/migrations/');
+
+        if (count($migrations) === count($files))
+            return true;
+
+        return false;
+    }
+
+    /**
+     * Check routes
+     *
+     * @return bool
+     */
+    private static function checkRoutes()
+    {
+        $blackList = ['health'];
+        $routes = Route::getRoutes()->getRoutesByMethod()['GET'];
+
+        foreach ($routes as $uri => $route) {
+            if (in_array($route->uri(), $blackList) || $route->getPrefix() === 'api') {
+                continue;
+            }
+
+            $response = app()->handle(Request::create(url($route->uri())));
+            $params = $route->parameters ?? [];
+
+            if (count($params)) continue;
+
+            $status = $response->status();
+            if ($status != 200) return false;
+        }
+
+        return true;
     }
 }
