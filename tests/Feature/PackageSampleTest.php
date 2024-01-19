7a25745088999c93d4fc4b7f065d77c0d33009d9
Mohammadreza_73 <mohammadreza.rahimi93@yahoo.com>
2024-01-19 23:57:29 +0330

    Modify tests
    Add unit file in directory,
    Fix feature test.

--- tests/Feature/PackageSampleTest.php
+++ tests/Feature/PackageSampleTest.php
@@ -8,11 +8,12 @@ class PackageSampleTest extends TestCase
 {
     public function testConfigFile()
     {
-        $this->assertFileExists(__DIR__.'/../config/health.php', 'فایل کانفیگ وجود ندارد');
+        $this->assertFileExists(dirname(__DIR__).'/../config/health.php', 'File not exists');
 
-        $array = include __DIR__.'/../config/health.php';
-        $this->assertArrayHasKey('database', $array, 'مقادیر کانفیگ به درستی ست نشده‌اند');
-        $this->assertArrayHasKey('migrations', $array, 'مقادیر کانفیگ به درستی ست نشده‌اند');
-        $this->assertArrayHasKey('routes', $array, 'مقادیر کانفیگ به درستی ست نشده‌اند');
+        $array = include dirname(__DIR__).'/../config/health.php';
+
+        $this->assertArrayHasKey('database', $array, 'Config values did not set true');
+        $this->assertArrayHasKey('migrations', $array, 'Config values did not set true');
+        $this->assertArrayHasKey('routes', $array, 'Config values did not set true');
     }
 }
